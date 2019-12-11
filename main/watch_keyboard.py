7475ef5cef34a9da08391412c8d79437d8fac904
Tomoya Suzuki <forever.blue.moon@gmail.com>
2019-12-11 16:08:48 +0900

    終了処理を追加した

--- main/watch_keyboard.py
+++ main/watch_keyboard.py
@@ -4,39 +4,53 @@ import sys
 from tkinter import *
 from tkinter import font,ttk
 
-def main():
-    timeout_count = [0]
+class watchKeyboard():
 
-    def watchKeyboard():
+    def __init__(self,argv):
+        super().__init__()
+        if len(argv)>1 :
+            self.watch_minutes = argv[1]
+        else:
+            self.watch_minutes = 5
+        self.watch_seconds = self.watch_minutes * 60 if self.watch_minutes > 0 else 300
+    
+    def main(self):
+        print('サボりチェッカーです。' + str(self.watch_minutes) + '分キータイプが無い場合に警告します。')
+        print('作業終了する場合はctrl + cを押してください')
+        try:
+            self.watch(0)
+            self.warning()
+        except KeyboardInterrupt:
+            print('作業終了ですね。お疲れ様でした。')
+            sys.exit(0)
+    
+    def watch(self,timeout_count):
         typed_pool = []
         keyboard.start_recording()
-        time.sleep(5)
+        time.sleep(self.watch_seconds)
         typed_pool = keyboard.stop_recording()
         if len(typed_pool) == 0:
-            timeout_count[0] += 1
+            timeout_count += 1
         else:
-            timeout_count[0] = 0
-        if timeout_count[0] <= 5:
-            time.sleep(60)
-            watchKeyboard()
+            timeout_count = 0
+        if timeout_count <= 5:
+            self.watch(timeout_count)
+        else:
+            return
     
-    watchKeyboard()
+    def warning(self):
+        root = Tk()
+        root.title(u"サボってる人チェッカー")
+        root.geometry("400x200")
 
-    # サボり認定！
-    # 基本情報
-    root = Tk()
-    root.title(u"サボってる人チェッカー")
-    root.geometry("400x200")
-
-    # メッセージ情報
-    msg_txt = StringVar()
-    msg_txt.set('サボりか？')
-    msg_font = font.Font(family='Helvetica', size=80, weight='bold')
-    msg = Label(root,textvariable=msg_txt,font=msg_font)
-    msg.pack()
-    root.mainloop()
-
-    root.protocol("WM_DELETE_WINDOW", main())
+        # メッセージ情報
+        msg_txt = StringVar()
+        msg_txt.set('サボりか？')
+        msg_font = font.Font(family='Helvetica', size=80, weight='bold')
+        msg = Label(root,textvariable=msg_txt,font=msg_font)
+        msg.pack()
+        root.mainloop()
+        root.protocol("WM_DELETE_WINDOW", self.watch(0))
 
 if __name__ == "__main__":
-    main()
+    watchKeyboard(sys.argv).main()
